a761ef56402ea34021d3e74bdd2496529afa7bfe
zekiriabd <zekiriabd@yahoo.fr>
2019-07-14 05:45:05 -0700

    Jquery single page

---
+++ Single Page/js/index.js
@@ -0,0 +1,33 @@
+﻿void function index_function(undefined) {
+
+ function loadControlTemplate(dest_id, fileUrl) {
+  var placeHolder = document.getElementById(dest_id);
+  Add_Html_Page(placeHolder, ToInnerHTML(fileUrl));
+ }
+
+ function Page1_click(e) {
+     $("#page1").removeClass("active");
+     $("#page2").addClass("active");
+ }
+
+ function Page2_click(e) {
+     $("#page2").removeClass("active");
+     $("#page1").addClass("active");
+ }
+ /*==============================================
+    - Get User info 
+    - Add user pages in master page 
+    ==============================================*/
+$(document).ready(function () {
+    loadControlTemplate('page1_content', "controls/page1.html");
+    loadControlTemplate('page2_content', "controls/page2.html");
+    //loadControlTemplate('left_bar_control', "controls/left_bar.html");
+    //loadControlTemplate('left_bar_control', "controls/left_bar.html");
+
+
+    $("#btn_page1").click(function (e) { Page1_click(e) });
+    $("#btn_page2").click(function (e) { Page2_click(e) });
+
+ });
+
+}.call();
---
+++ Single Page/js/lib/io.js
@@ -0,0 +1,38 @@
+﻿// *************************************************************** IO
+// http://codingforums.com/showthread.php?t=143412
+// LA MOD String Version. 
+// A tiny ajax library by DanDavis
+// Revised 20101006
+// http://www.quirksmode.org/js/xmlhttp.html
+
+var XMLHttpFactories = [
+	function ( ) { return ( new XMLHttpRequest ( ) );},
+	function ( ) { return ( new ActiveXObject ( "Msxml2.XMLHTTP" ) );},
+	function ( ) { return ( new ActiveXObject ( "Msxml3.XMLHTTP" ) );},
+	function ( ) { return ( new ActiveXObject ( "Microsoft.XMLHTTP" ) );}
+  ];
+
+function createXMLHTTPObject ( ) 
+  {
+  var xmlhttp = false;
+  for ( var i = 0; ( i < XMLHttpFactories.length ); i++ ) 
+    {
+    try{xmlhttp = XMLHttpFactories [ i ] ( );}
+    catch ( e ) {continue;}
+    break;
+    }
+  return ( xmlhttp );
+  }
+
+function ToInnerHTML  ( url ) 
+  {
+  var request = createXMLHTTPObject ( );
+  request.open ( 'GET', url, false );
+  request.setRequestHeader ( 'Content-Type', 'text/html' );
+  request.send ( '' );
+  return ( request.responseText );
+  }
+
+function Add_Html_Page(element,contents) {
+if (element.outerHTML) {element.outerHTML = contents;} else {element.innerHTML = contents;}
+}
